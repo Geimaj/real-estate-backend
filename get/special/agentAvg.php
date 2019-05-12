a7da703009eceefae398327e653712c39a415ddc
geimaj <16000925@vcconnect.co.za>
2019-05-12 19:54:29 +0200

    agent avg

--- get/special/agentAvg.php
+++ get/special/agentAvg.php
@@ -1,7 +1,9 @@
 <?php
 $year;
+$where = " where YEAR(Sale_Date) ";
 if (isset($_GET['year'])) {
     $year= $_GET['year'];
+    $where = $where . " = " . $year . " ";
 } else {
     $year=2018;
 }
@@ -10,37 +12,37 @@ header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 include("../../connection.php");
 
-$sql = "SELECT AVG(sales.Sale_Amount) AS AvSaleAmount,AVG(sales.Sales_TimeOnMarket) AS AvTime, person.Person_Firstname,person.Person_LastName
-FROM (agent
-INNER JOIN sales
-ON agent.Agent_ID=sales.Agent_Agent_ID)
-INNER JOIN person 
-ON person.Person_ID=agent.Agent_ID
-WHERE YEAR(sales.Sale_Date)=".$year."
-GROUP BY agent.Agent_ID
+$sql = "SELECT Agent_Agent_ID,
+person.Person_Firstname,
+person.Person_LastName,
+AVG(sales.Sale_Amount) as avgSales,
+AVG(sales.Sales_TimeOnMarket) as avgTimeOnMarket FROM `agent`
+join sales on sales.Agent_Agent_ID = agent.Agent_ID
+join `person` on agent.Person_Person_ID = person.Person_ID"
+. $where . "
+group by agent.Agent_ID
+order by avgSales DESC
+limit 1
 ";
 
-echo $sql;
 $result = $conn->query($sql);
-$jsonarray=array();
 if ($result->num_rows > 0) {
     // output data of each row
 	while($arrayresult = mysqli_fetch_array($result)) {
-       $myArray[] = array( 
-						"Agent_FirstName"=>$arrayresult['Person_Firstname'],
-						"Agent_LastName"=>$arrayresult['Person_LastName'],
-						"AvSaleAmount"=>$arrayresult['AvSaleAmount'],
-						"AvTime"=>$arrayresult['AvTime']
-                                           );
+       $myArray[] = array(
+            "firstname"=>$arrayresult['Person_Firstname'],
+            "lastname"=>$arrayresult['Person_LastName'],
+            "avgSales"=>$arrayresult['avgSales'],
+            "avgTimeOnMarket"=>$arrayresult['avgTimeOnMarket']
+        );
     }
 	// set response code - 200 OK
     http_response_code(200);
-
     echo json_encode($myArray);
 } else {
 // set response code - 404 Not found
     http_response_code(404);
- 
+
     // tell the user no products found
     echo json_encode(
         array("message" => "No properties found.")
--- get/stats/agents.php
+++ get/stats/agents.php
@@ -10,7 +10,15 @@ header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 include("../../connection.php");
 
-$sql = "SELECT Agent_Agent_ID, person.Person_Firstname, person.Person_LastName, sum(Sale_Amount) as totalValue, COUNT(Agent_Agent_ID) as totalSales FROM `agent`
+$sql = "SELECT
+Agent_Agent_ID,
+person.Person_Firstname,
+person.Person_LastName,
+sum(Sale_Amount) as totalValue,
+COUNT(Agent_Agent_ID) as totalSales,
+AVG(sales.Sale_Amount) as avgSales,
+AVG(sales.Sales_TimeOnMarket) as avgTimeOnMarket
+FROM `agent`
 join sales on sales.Agent_Agent_ID = agent.Agent_ID
 join person on agent.Person_Person_ID = person.Person_ID"
 . $where .
@@ -27,7 +35,9 @@ if ($result->num_rows > 0) {
             "firstname"=>$arrayresult['Person_Firstname'],
             "lastname"=>$arrayresult['Person_LastName'],
             "totalValueSold"=>$arrayresult['totalValue'],
-            "totalSales" => $arrayresult['totalSales']
+            "totalSales" => $arrayresult['totalSales'],
+            "avgSales"=>$arrayresult['avgSales'],
+            "avgTimeOnMarket"=>$arrayresult['avgTimeOnMarket']
         );
     }
 	// set response code - 200 OK
