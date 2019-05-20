2398b6c42300e7d32070368a4e87d43661f83707
geimaj <16000925@vcconnect.co.za>
2019-05-20 03:07:41 +0200

    person DOB format

--- post/person.php
+++ post/person.php
@@ -1,5 +1,3 @@
-
-
 <?php
 $body = file_get_contents('php://input');
 
@@ -15,10 +13,22 @@ header("Content-Type: application/json; charset=UTF-8");
 
 include("../connection.php");
 
-$sql = $conn->prepare("INSERT INTO `person`(`Address_Address_ID`, `Person_Firstname`, `Person_LastName`, `Person_PhoneNumber`, `Person_Email`, `Person_DOB`) VALUES (?,?,?,?,?,?)");
+$sql = $conn->prepare("INSERT INTO `person`(
+    `Address_Address_ID`,
+    `Person_Firstname`,
+    `Person_LastName`,
+    `Person_PhoneNumber`,
+    `Person_Email`,
+    `Person_DOB`) VALUES (?,?,?,?,?, STR_TO_DATE( ? , '%d-%m-%Y' ) )");
 
 if($sql != false){
-    $sql->bind_param("isssss", $person['address'], $person['firstname'],$person['firstname'],$person['phone'],$person['email'],$person['dob']);
+    $sql->bind_param("isssss",
+    $person['address'],
+     $person['firstname'],
+     $person['firstname'],
+     $person['phone'],
+     $person['email'],
+     $person['dob']);
     $result = $sql->execute();
 
     if($result===TRUE){
@@ -28,17 +38,17 @@ if($sql != false){
         $person_id=$conn->insert_id;
 
         //check if we should insert person_id into buyer, seller or agent
-        if($person['buyer'] == TRUE){
+        if($person['buyerID'] == TRUE){
             $sql = $conn->prepare("INSERT INTO `buyer`(`Person_Person_ID`) VALUES (?)");
             $sql->bind_param("i", $person_id);
         }
 
-        if($person['seller'] == TRUE){
+        if($person['sellerID'] == TRUE){
             $sql = $conn->prepare("INSERT INTO `seller`(`Person_Person_ID`) VALUES (?)");
             $sql->bind_param("i", $person_id);
         }
 
-        if($person['agent'] == TRUE){
+        if($person['agentID'] == TRUE){
             $sql = $conn->prepare("INSERT INTO `agent`(`Person_Person_ID`) VALUES (?)");
             $sql->bind_param("i", $person_id);
         }
@@ -61,6 +71,7 @@ if($sql != false){
 
 // set response code - 404 Not found
     http_response_code(404);
+    echo $sql->error;
     print_r( $conn->error_list );
 
 $conn->close();
